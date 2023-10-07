d428a1f27a59be2b952c808454de33be5a7402fa
sultaale <impa1387@gmail.com>
2023-10-07 18:55:08 +0300

    Add kitchen service

---
+++ kitchen-service/src/main/java/ru/liga/KitchenService.java
@@ -0,0 +1,13 @@
+package ru.liga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenService.class, args);
+	}
+
+}
---
+++ kitchen-service/src/test/java/ru/liga/KitchenServiceTests.java
@@ -0,0 +1,13 @@
+package ru.liga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
