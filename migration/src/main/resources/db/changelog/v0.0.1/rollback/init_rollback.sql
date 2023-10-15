a96ac224cb9b5464a517aaae7971570c3c5ce584
sultaale <impa1387@gmail.com>
2023-10-15 18:34:08 +0300

    Remove sequences

--- migration/src/main/resources/db/changelog/v0.0.1/rollback/init_rollback.sql
+++ migration/src/main/resources/db/changelog/v0.0.1/rollback/init_rollback.sql
@@ -1,23 +1,12 @@
 drop table if exists customers;
 
-drop sequence if exists customers_seq;
-
 drop table if exists couriers;
-
-drop sequence if exists couriers_seq;
 
 drop table if exists restaurants;
 
-drop sequence if exists restaurants_seq;
-
 drop table if exists restaurant_menu_items;
-
-drop sequence if exists restaurant_menu_items_seq;
 
 drop table if exists orders;
 
-drop sequence if exists orders_seq;
-
 drop table if exists order_items;
 
-drop sequence if exists order_items_seq;
