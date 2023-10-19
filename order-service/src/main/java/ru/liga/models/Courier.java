2eacda236272b1c8bda444d87db7b161cabeba15
sultaale <impa1387@gmail.com>
2023-10-19 16:04:04 +0300

    Add models for all tables

--- order-service/src/main/java/ru/liga/models/Courier.java
+++ order-service/src/main/java/ru/liga/models/Courier.java
@@ -1,14 +1,63 @@
 package ru.liga.models;
 
-import lombok.Getter;
-import lombok.Setter;
-import java.awt.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.postgresql.geometric.PGpoint;
+import ru.liga.util.PGPointType;
 
-@Getter
-@Setter
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@TypeDef(name = "type", typeClass = PGPointType.class)
+@Table(name = "couriers")
 public class Courier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "courier_seq_gen")
+    @SequenceGenerator(name = "courier_seq_gen", sequenceName = "couriers_seq", allocationSize = 1)
     private Long id;
     private String phone;
     private String status;
-    private Point point;
+    @Type(type = "type")
+    private PGpoint coordinates;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public PGpoint getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(PGpoint coordinates) {
+        this.coordinates = coordinates;
+    }
 }
--- order-service/src/main/java/ru/liga/models/RestaurantMenuItem.java
+++ order-service/src/main/java/ru/liga/models/RestaurantMenuItem.java
@@ -1,15 +1,44 @@
 package ru.liga.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Setter
 @Getter
+@Table(name = "restaurant_menu_items")
 public class RestaurantMenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "rest_menu_items_seq_gen")
+    @SequenceGenerator(name = "rest_menu_items_seq_gen", sequenceName = "restaurant_menu_items_seq", allocationSize = 1)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
+
     private String name;
-    private Long price;
-    private String image; //TODO уточнить как хранится картинка, пока это путь к картинке
+
+    private Double price;
+
+    private String image;
+
     private String description;
+
+    @OneToOne(mappedBy = "restaurantMenuItem")
+    private OrderItem orderItem;
 }
